cd9389323f69772f9b1328d1ab5accf8b6711935
SnowyyUwU <ulisesdraws@gmail.com>
2022-09-10 18:34:24 +0000

    Initial commit

---
+++ src/main/java/com/snowy/snowydatabase/SnowyDatabase.java
@@ -0,0 +1,13 @@
+package com.snowy.snowydatabase;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SnowyDatabase extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+}
